9beb96b259819d2f5de1895f66a0c4dd13f469e7
Jhon Castillo <gpro1pro@gmail.com>
2022-09-13 00:18:42 -0500

    new version

--- administrador/conexion.php
+++ administrador/conexion.php
@@ -4,6 +4,7 @@
 	$password="";
 	$db="convocatorias";
 	$conn=new mysqli($servername,$username,$password,$db);
+	$conn->set_charset("utf8");
 	if($conn->connect_error){
 		die("conecion faliida: " .$conn->connect_error);
 	}
